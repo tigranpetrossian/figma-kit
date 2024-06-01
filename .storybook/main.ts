8b17210bd5a986eb7635dc6f6b76f8329759c065
Tigran <tgpett@gmail.com>
2024-06-01 17:37:43 +0400

    chore: add dark mode to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    'storybook-dark-mode',
   ],
   framework: {
     name: '@storybook/react-vite',
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from '@storybook/react';
+import './storybook-reset.css';
+import '../figma-primitives/src/figma-tokens.css';
 
 const preview: Preview = {
   parameters: {
@@ -7,6 +9,12 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    darkMode: {
+      darkClass: 'dark',
+      lightClass: 'light',
+      classTarget: 'html',
+      stylePreview: true,
     },
   },
 };
