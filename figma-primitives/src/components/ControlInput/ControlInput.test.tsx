02a62241c2bb8a4795fbb9e6cff3b9fffeae807e
Tigran <tgpett@gmail.com>
2024-06-05 11:59:42 +0400

    test: add test todos

---
+++ figma-primitives/src/components/ControlInput/ControlInput.test.tsx
@@ -0,0 +1,11 @@
+import { it } from 'vitest';
+
+it.todo('reverts invalid values', () => {});
+it.todo("doesn't call onChange when input is same as current value", () => {});
+it.todo('parses correctly', () => {});
+it.todo('clamps correctly', () => {});
+it.todo('formats correctly', () => {});
+it.todo('saves on Enter', () => {});
+it.todo('saves on blur', () => {});
+it.todo('reverts on Escape', () => {});
+it.todo("doesn't close parent modal on Escape", () => {});
