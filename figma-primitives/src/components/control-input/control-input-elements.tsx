90d91bba7babfb8836fc63484b338e03da898e41
Tigran <tgpett@gmail.com>
2024-06-11 17:23:56 +0400

    fix: improve control input root styling

--- figma-primitives/src/components/control-input/control-input-elements.tsx
+++ figma-primitives/src/components/control-input/control-input-elements.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { cx } from 'class-variance-authority';
 
-type RootElement = React.ElementRef<'div'>;
-type RootProps = React.ComponentPropsWithoutRef<'div'>;
+type RootElement = React.ElementRef<'label'>;
+type RootProps = React.ComponentPropsWithoutRef<'label'>;
 
 const Root = React.forwardRef<RootElement, RootProps>((props, ref) => {
   const { className, ...rootProps } = props;
-  return <div ref={ref} className={cx(className, 'fp-ControlInputRoot')} {...rootProps} />;
+  return <label ref={ref} className={cx(className, 'fp-ControlInputRoot')} {...rootProps} />;
 });
 
+export type { RootProps };
 export { Root };
--- figma-primitives/src/components/control-input/index.ts
+++ figma-primitives/src/components/control-input/index.ts
@@ -1,1 +1,4 @@
-export * from './control-input-base';
+export { Root } from './control-input-elements';
+export type { RootProps } from './control-input-elements';
+export { Numeric } from './control-input-numeric';
+export type { NumericProps } from './control-input-numeric';
