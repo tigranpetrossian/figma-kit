5cf53f61d18a247530255f8efdb6c893ca0f4166
Tigran <tgpett@gmail.com>
2024-06-20 00:17:11 +0400

    refactor: merge input base with input

--- figma-kit/src/components/input/input.tsx
+++ figma-kit/src/components/input/input.tsx
@@ -1,22 +1,49 @@
 import React, { useRef } from 'react';
 import mergeProps from 'merge-props';
-import { InputBase } from '@components/input-base';
+import { cva, type VariantProps } from 'class-variance-authority';
 import { useSelectOnInputClick } from '@lib/react/use-select-on-input-click';
 import { useComposedRefs } from '@lib/react/use-compose-refs';
 
+const inputBase = cva('fp-inputBase', {
+  variants: {
+    variant: {
+      /**
+       * Unstyled variant for constructing mode complex fields.
+       * */
+      base: 'base',
+      /**
+       * Borderless, unless interacted with.
+       * */
+      ghost: 'ghost',
+      /**
+       * Common bordered input.
+       * */
+      normal: 'normal',
+      /*
+       * Bottom border only, unless interacted with.
+       * */
+      underline: 'underline',
+    },
+  },
+  defaultVariants: {
+    variant: 'normal',
+  },
+});
+
 type InputElement = React.ElementRef<'input'>;
-type InputProps = React.ComponentPropsWithoutRef<typeof InputBase> & {
-  selectOnClick?: boolean;
-};
+type InputProps = React.ComponentPropsWithoutRef<'input'> &
+  VariantProps<typeof inputBase> & {
+    selectOnClick?: boolean;
+  };
 
 const Input = React.forwardRef<InputElement, InputProps>((props, forwardedRef) => {
-  const { selectOnClick = false, ...rest } = props;
+  const { type = 'text', variant, className, selectOnClick = false, ...rest } = props;
   const ref = useRef<HTMLInputElement>(null);
   const composedRef = useComposedRefs(ref, forwardedRef);
   const { onMouseLeave, onMouseUp, onFocus } = useSelectOnInputClick();
   const inputProps = selectOnClick ? mergeProps({ onMouseLeave, onMouseUp, onFocus }, rest) : rest;
 
-  return <InputBase ref={composedRef} {...inputProps} />;
+  return <input ref={composedRef} type={type} className={inputBase({ className, variant })} {...inputProps} />;
 });
 
 export type { InputProps };
