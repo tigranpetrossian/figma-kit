82b881ad6fb9421f63b818df965baea112696791
Tigran <tgpett@gmail.com>
2024-06-21 18:23:16 +0400

    refactor: improve thumb focus visibility handling logic

--- figma-kit/src/components/slider/slider.tsx
+++ figma-kit/src/components/slider/slider.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import * as RadixSlider from '@radix-ui/react-slider';
-import { cx, cva } from 'class-variance-authority';
+import { cx } from 'class-variance-authority';
 import { useComposedRefs } from '@lib/react/use-compose-refs';
 
 type SliderElement = React.ElementRef<typeof RadixSlider.Root>;
@@ -25,7 +25,7 @@ const Slider = React.forwardRef<SliderElement, SliderProps>((props, forwardedRef
   } = props;
   const rootRef = useRef<SliderElement>(null);
   const ref = useComposedRefs(rootRef, forwardedRef);
-  const { onPointerDown, pointerDown } = useSliderFocusVisible(rootRef);
+  const { onPointerDown, focusVisible } = useSliderVisibleFocus(rootRef);
 
   return (
     <>
@@ -47,28 +47,22 @@ const Slider = React.forwardRef<SliderElement, SliderProps>((props, forwardedRef
           <div className="fp-sliderRange" data-orientation={orientation} />
         </RadixSlider.Track>
         {(value ?? defaultValue).map((v, i) => (
-          <RadixSlider.Thumb key={i} className={thumb({ pointerDown })} onPointerDown={onPointerDown} />
+          <RadixSlider.Thumb
+            key={i}
+            className={cx('fp-sliderThumb', { 'fp-sliderThumb-focusVisible': focusVisible })}
+            onPointerDown={onPointerDown}
+          />
         ))}
       </RadixSlider.Root>
     </>
   );
 });
 
-const thumb = cva('fp-sliderThumb', {
-  variants: {
-    pointerDown: {
-      true: 'fp-sliderThumb-pointerDown',
-    },
-  },
-});
-
 /**
- * Match the :focus-visible behavior of the slider thumb to that of normal buttons.
- * The thumb, a span with tabindex="0", displays an outline ring even when dragged with a mouse.
- * This fix only shows the focus ring when the pointer isn't down.
- * After mouse use, focus remains on the Root element to enable subsequent keyboard interactions.
+ * Fix :focus-visible behavior for the slider thumb, to only show outline when not dragging with the mouse.
+ * The focus will remain on the Root element afterward to enable subsequent keyboard interactions.
  */
-function useSliderFocusVisible(ref: React.RefObject<SliderElement>) {
+function useSliderVisibleFocus(ref: React.RefObject<SliderElement>) {
   const [pointerDown, setPointerDown] = useState(false);
 
   const handleGlobalUp = () => {
@@ -82,7 +76,7 @@ function useSliderFocusVisible(ref: React.RefObject<SliderElement>) {
     window.addEventListener('pointerup', handleGlobalUp);
   };
 
-  return { onPointerDown: handlePointerDown, pointerDown };
+  return { onPointerDown: handlePointerDown, focusVisible: !pointerDown };
 }
 
 export type { SliderProps };
